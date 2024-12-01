33f7f2f6a4ac266f5198d6f018ca9c6b625f5c6e
Michał Dubowski <mdubowski@mdubowski.pl>
2024-12-01 15:59:28 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return Day01(input).part1()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return Day01(input).part2()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+class Day01(private val input: List<String>) {
+    val groups
+        get() = input.map { it.lineToPair() }.unzip()
+
+    fun part1(): Int {
+        val (firstGroup, secondGroup) = groups
+            .let { it.first.sorted() to it.second.sorted() }
+
+        return firstGroup.zip(secondGroup)
+            .sumOf { abs(it.first - it.second) }
+    }
+
+    fun part2(): Int {
+        val (firstGroup, secondGroup) = groups
+        val secondGroupLocationsToOccurrences = secondGroup.groupingBy { it }.eachCount()
+
+        return firstGroup.sumOf { it * secondGroupLocationsToOccurrences.getOrDefault(it, 0) }
+    }
+
+    private fun String.lineToPair() = split(" ").mapNotNull { it.toInteger() }.let { Pair(it[0], it[1]) }
+
+    private fun String.toInteger(): Int? = runCatching { toInt() }.getOrNull()
 }
